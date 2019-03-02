1439ccc1f1ee9dfcd021658e64ee6f13f547e4b9
Francesco Done <francesco.done@studenti.unipd.it>
2019-03-02 15:31:50 +0100

    update login

--- src/components/containers/NotLogged.js
+++ src/components/containers/NotLogged.js
@@ -2,13 +2,13 @@ import { connect } from 'react-redux';
 import { logIn, logOut } from '../../actions/login';
 import LogButton from '../presentational/LogButton';
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return {
     logged: state.logged
   }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   return {
     logIn: (log) => {
       dispatch(logIn(log))
