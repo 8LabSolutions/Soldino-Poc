c4bc684056262af218d9b1564b0429f35b718656
Samuele <samuele.piazzetta@gmail.com>
2019-03-21 11:52:20 +0100

    fatto niente

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,4 +9,5 @@ module.exports = function(deployer) {
   //deployer.deploy(Accounts);
   //deployer.deploy(Token, 10000, "Cubit", "CC", "0xA5A91DE72568141687864507423604f9Ea25E823");
   deployer.deploy(Logic);
+
 };
--- test/proxy_test.js
+++ test/proxy_test.js
@@ -3,45 +3,16 @@ const web3 = getWeb3()
 const getInstance = getContractInstance(web3)
 
 
-
 contract("Proxy", (accounts) => {
-    var proxy = getInstance('Proxy', accounts[1])
-    var logic = getInstance('UserLogic', accounts[1])
-    var logic2 = getInstance('UserLogicV2', accounts[1])
+    var logic = getInstance('UserLogic', accounts[2])
     it("check user registration", async () => {
-        const logicInstance = await logic.deploy().send()
-        const logicInstance2 = await logic.deploy().send()
-        const proxyInstance = await proxy.deploy().send()
+        const logicInstance = await logic.deploy().send() //per registrare un cittadino
 
-        console.log("Logic: "+logicInstance.options.address)
-        console.log("Proxy: "+proxyInstance.options.address)
-        console.log("LogicV2: "+logicInstance2.options.address)
-
-        let imp = await proxyInstance.methods._implementation().call()
-
-        assert.equal('0x0000000000000000000000000000000000000000', imp)
-
-        await proxyInstance.methods.upgradeTo(logicInstance.options.address).send()
-        imp = await proxyInstance.methods._implementation().call()
-
-        assert.equal(imp, logicInstance.options.address)
-
-        logicInstance.options.address = proxyInstance.options.address
-
-        await logicInstance.methods.registerBusiness(
-          accounts[2],
+        await logicInstance.methods.registerCitizen('0x0000000000000000000000000000000000000000',
           '0x0000000000000000000000000000000000000000',
           '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000')
-        .send()
-
-        let reg = await logicInstance.methods.testUpgrade().call()
-        assert.equal(reg.toString(), 'true')
-
-
-        await proxyInstance.methods.upgradeTo(logicInstance2.options.address).send()
-        reg = await proxyInstance.methods.testUpgrade().call()
-        assert.equal(reg.toString(), 'false')
+          '0x0000000000000000000000000000000000000000').send({from: accounts[5]});
+        let value = await logicInstance.methods.userIsRegistered(accounts[5]).call();
+        assert.equal("2", value.toString());
     })
 })
